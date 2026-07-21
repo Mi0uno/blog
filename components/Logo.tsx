8fb39f53c7ce4ed0d4215b778f91284e8c4890d3
klopiop <2062769036@qq.com>
2026-07-21 17:28:57 +0800

    feat: update blog logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -7,17 +7,11 @@ interface LogoProps {
 
 export const Logo: React.FC<LogoProps> = ({ className }) => {
   return (
-    <motion.svg
+    <motion.img
       layout
-      viewBox="0 0 100 100"
-      fill="currentColor"
-      xmlns="http://www.w3.org/2000/svg"
+      src="/logo.png"
+      alt="mi0034 Logo"
       className={className}
-      aria-label="mi0034 Logo"
-      role="img"
-    >
-      <title>mi0034 Logo</title>
-      <path d="M15 15 V 85 H 30 V 40 L 50 60 L 70 40 V 85 H 85 V 15 L 50 50 L 15 15 Z" />
-    </motion.svg>
+    />
   );
 };
